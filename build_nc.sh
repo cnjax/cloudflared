e61a9cc7d97e3a99404e970a1203e46f977c33be
nightcat <nightcat@gmail.com>
2025-01-18 16:06:26 +0800

    update make file

--- build_nc.sh
+++ build_nc.sh
@@ -1,2 +1,8 @@
-GOOS=linux make cloudflared
+export GOEXPERIMENT=noboringcrypto
+export CGO_ENABLED=0
+
+# This controls the directory the built artifacts go into
+export ARTIFACT_DIR=artifacts/
+export GOOS=linux
+make cloudflared
 
